e3d8644fe8241611ab76b28c0bd913e59a2fb2c6
jay <jayandcode@googlemail.com>
2018-03-09 15:34:05 +0000

    Add Styles

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 window.addEventListener("keyup", function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`key[data-key="${e.keyCode}"]`);
+  
   if(!audio){
     return;
   }
   else {
     audio.currentTime = 0;
+
     audio.play();
   }
 
