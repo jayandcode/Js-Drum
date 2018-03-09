3ca5f2f7f74bd35fcede1aff53a23b1aa0dc60c6
jay <jayandcode@googlemail.com>
2018-03-09 15:41:54 +0000

    Finish Js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 window.addEventListener("keyup", function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`key[data-key="${e.keyCode}"]`);
-  
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio){
     return;
   }
   else {
+    resetStyle();
     audio.currentTime = 0;
-
+    key.classList.add("active");
     audio.play();
   }
 
 });
+
+function resetStyle(){
+  const allKeys = document.querySelectorAll(".key");
+  allKeys.forEach(function(key){
+  key.classList.remove("active");
+  });
+}
